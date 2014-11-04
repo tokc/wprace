ba02afd59906be7e17fcf3785bc2c8c18101077d
tokc <eviloatmeal@eviloatmeal.com>
2014-11-04 11:38:05 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Title</title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	</head>
+
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<h1>HEADER</h1>
+			</div>
