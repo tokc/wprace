21f162af64b3cd812540c08c8f731c66275d2db0
tokc <eviloatmeal@eviloatmeal.com>
2014-11-04 14:44:14 +0100

    Wordpress adds <p> inside my <p>. Woops!

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<h1><?php the_title(); ?></h1>
 		<h4>Posted on <?php the_time('F jS, Y') ?></h4>
-		<p><?php the_content(__('(more...)')); ?></p>
+		<?php the_content(__('(more...)')); ?>
 		<hr> <?php endwhile; else: ?>
 		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
 	</div>
